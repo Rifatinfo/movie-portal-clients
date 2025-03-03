80478bf6281141a48e66263dc03bc100ebbd2184
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-03-03 12:59:29 +0600

    navber overall correction

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon , XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    { name: 'Home', href: '/', current: true },
+    { name: 'All Movies', href: 'All-Movies', current: false },
+    { name: 'Add Movie', href: 'Add-Movie', current: false },
+    { name: 'My Favorites', href: 'My-Favorites', current: false },
 ]
 
 function classNames(...classes) {
@@ -50,19 +50,18 @@ const Navbar = () => {
                                         >
                                             {item.name}
                                         </a>
+
                                     ))}
+
                                 </div>
                             </div>
                         </div>
                         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                            <button
-                                type="button"
-                                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
-                            >
-                                <span className="absolute -inset-1.5" />
-                                <span className="sr-only">View notifications</span>
-                                <BellIcon aria-hidden="true" className="size-6" />
-                            </button>
+                            
+                            <div className='md:flex md:gap-2 hidden'>
+                                <button className="rounded-4xl btn bg-white text-black">Sign In</button>
+                                <button className="rounded-4xl btn bg-white text-black">Register</button>
+                            </div>
 
                             {/* Profile dropdown */}
                             <Menu as="div" className="relative ml-3">
@@ -77,35 +76,7 @@ const Navbar = () => {
                                         />
                                     </MenuButton>
                                 </div>
-                                <MenuItems
-                                    transition
-                                    className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-                                >
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                                        >
-                                            Your Profile
-                                        </a>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                                        >
-                                            Settings
-                                        </a>
-                                    </MenuItem>
-                                    <MenuItem>
-                                        <a
-                                            href="#"
-                                            className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
-                                        >
-                                            Sign out
-                                        </a>
-                                    </MenuItem>
-                                </MenuItems>
+                                
                             </Menu>
                         </div>
                     </div>
@@ -127,6 +98,10 @@ const Navbar = () => {
                                 {item.name}
                             </DisclosureButton>
                         ))}
+                        <div className='flex gap-2 md:hidden'>
+                            <button className="rounded-4xl btn bg-white text-black">Sign In</button>
+                            <button className="rounded-4xl btn bg-white text-black">Register</button>
+                        </div>
                     </div>
                 </DisclosurePanel>
             </Disclosure>
